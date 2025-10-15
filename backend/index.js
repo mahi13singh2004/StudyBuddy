550a3a50b217d9eb4b93953de5841a5be3354df1
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-10-15 18:08:15 +0530

    added db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import connectDB from "./db/connectDB.js"
 dotenv.config()
 
 const app=express()
@@ -10,5 +11,6 @@ app.use(cookieParser())
 const PORT=process.env.PORT || 5000
 
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`Server is running on port ${PORT}`)
 })
