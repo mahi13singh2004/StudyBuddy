bfa686824eddff8df6c801121700d1fe912951ec
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-10-20 00:54:43 +0530

    removed env

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://studybuddy-bz2d.onrender.com",
+    baseURL: "https://studybuddy-bz2d.onrender.com",
     withCredentials: true,
 })
 
