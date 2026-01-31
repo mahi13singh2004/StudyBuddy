d5398be07047de6d76eb0be9cccce9e895ea445b
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-01-31 21:41:51 +0530

    try demo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -149,7 +149,7 @@ const Login = () => {
                     <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                     </svg>
-                    <span>Try As Recruiter</span>
+                    <span>Try Demo Account</span>
                   </button>
                 </div>
               </form>
